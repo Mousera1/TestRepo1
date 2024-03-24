bd4e7a3c0e2b2c302682d778481eb2137bd56837
mousera1 <mouricesera1@gmail.com>
2024-03-24 13:54:52 +0530

    commit done

---
+++ JavaAutomation07/src/javapackage/JavaAutomation.java
@@ -0,0 +1,10 @@
+package javapackage;
+
+public class JavaAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aotomation");
+	}
+
+}
